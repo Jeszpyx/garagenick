4e5a965f715e0debc3b60c0dc9bb011adc950353
Jeszpyx <hleb.lukashonak@yandex.ru>
2024-07-04 22:18:45 +0300

    feat: db module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiConfigModule } from './modules/api-config/api-config.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ApiConfigModule],
+  imports: [ApiConfigModule, PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,7 @@ async function main() {
       transform: true
     }),
   );
+  app.enableShutdownHooks()
   
   const configService = app.get(ApiConfigService);
   const port = configService.get('PORT');
